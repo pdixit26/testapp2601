d8b4c31a962f0b38220f025dbc2d8ae60356828b
pdixit26 <pdixit26@gmail.com>
2018-11-26 23:56:48 -0800

    f

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ let pg = require('pg');
 const PORT = 3000; 
 
 let pool = new pg.Pool({
-	user:'gdfzlsijuexbdp',
-	database: 'd4g1iadabopnqg',
-	password: 'd30cae52f8dd8ac0b3fcdf6618fc1cd3f2779138493e6d313209c5ec20de1c9e',
-	host:'ec2-54-235-193-0.compute-1.amazonaws.com',
+	user:'gnqjtcrzjtfwqd',
+	database: 'd25knk4v6tpvja',
+	password: '0ff51153df38137a800d7dc3980aa0d5c82a16b671a8991d83eed7d17ba5afa4',
+	host:'ec2-54-197-234-33.compute-1.amazonaws.com',
 	port: 5432,
-	URI:'postgres://gdfzlsijuexbdp:d30cae52f8dd8ac0b3fcdf6618fc1cd3f2779138493e6d313209c5ec20de1c9e@ec2-54-235-193-0.compute-1.amazonaws.com:5432/d4g1iadabopnqg'
+	URI:'postgres://gnqjtcrzjtfwqd:0ff51153df38137a800d7dc3980aa0d5c82a16b671a8991d83eed7d17ba5afa4@ec2-54-197-234-33.compute-1.amazonaws.com:5432/d25knk4v6tpvja'
 });
 
 pool.connect((err, db, done) =>{
@@ -30,7 +30,7 @@ pool.connect((err, db, done) =>{
 		var C2R1 = '5';
 		var C2R2 = '6';
 		//db.query('INSERT INTO DeviceTable (tableid,devicetype,c0r0,c0r1,c0r2,c1r0,c1r1,c1r2,c2r0,c2r1,c2r2) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)', [tableid,devicetype,C0R0,C0R1,C0R2,C1R0,C1R1,C1R2,C2R0,C2R1,C2R2], (err, table) => {
-		db.query('CREATE TABLE pooja (id serial PRIMARY KEY,name VARCHAR (50))', (err, table) => {
+		db.query('CREATE TABLE devicetable(tableid NOT NULL,devicetype character varying NOT NULL,c1r0 numeric NOT NULL,c1r1 numeric NOT NULL,c1r2 numeric NOT NULL,c2r0 numeric NOT NULL,c2r1 numeric NOT NULL,c2r2 numeric NOT NULL,c0r0 numeric NOT NULL,c0r1 numeric NOT NULL,c0r2 numeric NOT NULL,CONSTRAINT "DeviceTable_pkey" PRIMARY KEY (tableid))', (err, table) => {
 			if(err)
 			{
 				return console.log("dixit "+ err);
