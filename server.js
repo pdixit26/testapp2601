5e42186275b9ed2a4592382db1b76a10369e8fb1
pdixit26 <pdixit26@gmail.com>
2018-11-27 00:02:41 -0800

    changes

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ pool.connect((err, db, done) =>{
 		var C2R1 = '5';
 		var C2R2 = '6';
 		//db.query('INSERT INTO DeviceTable (tableid,devicetype,c0r0,c0r1,c0r2,c1r0,c1r1,c1r2,c2r0,c2r1,c2r2) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)', [tableid,devicetype,C0R0,C0R1,C0R2,C1R0,C1R1,C1R2,C2R0,C2R1,C2R2], (err, table) => {
-		db.query('CREATE TABLE devicetable(tableid NOT NULL,devicetype character varying NOT NULL,c1r0 numeric NOT NULL,c1r1 numeric NOT NULL,c1r2 numeric NOT NULL,c2r0 numeric NOT NULL,c2r1 numeric NOT NULL,c2r2 numeric NOT NULL,c0r0 numeric NOT NULL,c0r1 numeric NOT NULL,c0r2 numeric NOT NULL,CONSTRAINT "DeviceTable_pkey" PRIMARY KEY (tableid))', (err, table) => {
+		db.query('CREATE TABLE devicetable(tableid numeric NOT NULL,devicetype varchar(255) NOT NULL,c1r0 numeric NOT NULL,c1r1 numeric NOT NULL,c1r2 numeric NOT NULL,c2r0 numeric NOT NULL,c2r1 numeric NOT NULL,c2r2 numeric NOT NULL,c0r0 numeric NOT NULL,c0r1 numeric NOT NULL,c0r2 numeric NOT NULL,CONSTRAINT "DeviceTable_pkey" PRIMARY KEY (tableid))', (err, table) => {
 			if(err)
 			{
 				return console.log("dixit "+ err);
