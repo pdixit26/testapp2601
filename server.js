389ba0fa31cdca6203153f3649a9e93af8c7b61d
pdixit26 <pdixit26@gmail.com>
2018-11-26 23:08:20 -0800

    second

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // in sublime
-let express = require(‘express’);
+let express = require('express');
 let port = process.env.PORT || 3000;
 let app = express();
-app.get(‘/’, function (req, res) {
- res.send(JSON.stringify({ Hello: ‘World’}));
+app.get('/', function (req, res) {
+ res.send(JSON.stringify({ Hello: 'World'}));
 });
 app.listen(port, function () {
- console.log(`Example app listening on port !`);
+ console.log('Example app listening on port !');
 });
